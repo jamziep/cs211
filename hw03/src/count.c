91bb9798e6ad994fd312270047fd5151868b346f
Lance Christopher Go <lcg3895@ras.ece.northwestern.edu>
2021-04-21 16:57:39 -0500

    count.c

--- hw03/src/count.c
+++ hw03/src/count.c
@@ -8,11 +8,56 @@
 #define DROP_MESSAGE    "%s: vote dropped: %s\n"
 #define FINAL_MESSAGE   "%s: %zu vote(s) dropped\n"
 
+
+// count.c takes candidate names from stdinput
+// also drops names once it has more names than can handle
+
+
+
+
 int main(int argc, char* argv[])
 {
-    //
-    // TODO: your code goes here.
-    //
+    
+    vote_count_t c_pool = vc_create();
+    if(!c_pool){
+        fprintf(stderr, OOM_MESSAGE, "./count");
+        return 1;
+    }
+    
+    size_t num_cands = 0;
+    size_t dropped_votes = 0;
+
+    while(1){             // this is running in an infinite loop for now
+        const char* name = read_line();
+
+        if(num_cands < MAX_CANDIDATES || vc_lookup(c_pool, name) != 0){
+            //will count votes as long as MAX has not been exceeded.
+            // if max is reached then checks if candidate is already in the pool. If not, drops the candidate
+
+            
+            size_t* count_ptr = vc_update(c_pool, name);
+        
+            if(vc_lookup(c_pool, name) == 0){     // if the candidate has yet to recieve any of the votes, they are a new candidate and num_cands needs to be incremented
+                ++num_cands;   
+            }
+        
+            *count_ptr +=  1;   // add one to the count for curr candidate
+        
+            printf("count : %ld\n", *count_ptr);        // these prints were just for debugging
+            printf("number of cands: %ld\n", num_cands);            
+        }else{
+            printf(DROP_MESSAGE, "./count", name);
+            ++dropped_votes;
+            
+        }
+
+
+        
+    }
+
+    vc_destroy(c_pool);
+       
+    
 
     //comment here
 }
