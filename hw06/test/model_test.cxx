a05060ee9efbf905a225e21066e3fec8dc6cd6c6
lancego2023 <lancego2023@u.northwestern.edu>
2021-05-20 16:30:50 -0500

    tests for flip/evaluate. Compute_next_moves test still broken

--- hw06/test/model_test.cxx
+++ hw06/test/model_test.cxx
@@ -36,6 +36,124 @@ TEST_CASE("Throws with starter code")
 
 // YOU NEED MANY MORE TESTS!
 
+struct Test_access
+{
+    Model& model;
+    Board& board()
+    {
+        return model.board_;
+    }
+
+    Position_set
+    find_flips(Model::Position p,
+               Model::Dimensions d)
+    {
+        return model.find_flips_(p,d);
+    }
+
+    Position_set
+    evaluate_position(Model::Position p)
+    {
+        return model.evaluate_position_(p);
+    }
+
+    // unlike, compute_next_moves_, compute_next will return a position set
+    // with all possible moves given the board. This is for testing purposes.
+    Position_set compute_next()
+    {
+        model.next_moves_.clear();
+        model.compute_next_moves_();
+        Position_set posns {};
+        for (const auto & [pos, flips] : model.next_moves_){
+                posns[pos] = true;
+        }
+        return posns;
+    }
+};
+
+// tests find_flips_ in model.cxx
+TEST_CASE("flip check")
+{
+    Model model;
+    Test_access t{model};
+
+    t.board()[{2,2}] = Player::dark;
+    t.board()[{2,3}] = Player::light;
+
+    Position_set f;
+
+    // simple flip check
+    f = t.find_flips({2,4}, {0,1});
+    CHECK(f.empty());
+    f = t.find_flips({2,4}, {0, -1});
+    CHECK(f == Position_set{{2,3}});
+    // diagonal flip check
+    f.clear();
+    t.board()[{3,2}] = Player::dark;
+    f = t.find_flips({1,4},{1,1});
+    CHECK(f.empty());
+    f = t.find_flips({1, 4}, {1, -1});
+    CHECK(f == Position_set{{2,3}});
+    //isolated tile
+    f.clear();
+    f = t.find_flips({4,4}, {1,1});
+    f = t.find_flips({4,4}, {0,1});
+    f = t.find_flips({4,4}, {1,0});
+    f = t.find_flips({4,4}, {-1,-1});
+    CHECK(f.empty());
+    //multiple tiles
+    f.clear();
+    t.board()[{1,2}] = Player::dark;
+    t.board()[{2,2}] = Player::light;
+    t.board()[{3,2}] = Player::light;
+    t.board()[{4,2}] = Player::light;
+    f = t.find_flips({5,2}, {1,0});
+    CHECK(f.empty());
+    f = t.find_flips({5,2}, {-1,0});
+    CHECK(f == Position_set{{2,2}, {3,2}, {4,2}});
+}
+
+
+// tests evaluate_position_ in model.cxx
+TEST_CASE("evaluating positions")
+{
+    Model model;
+    Test_access t{model};
+    Position_set f;
+
+    //initialize start positions
+    t.board()[{4,4}] = Player::dark;
+    t.board()[{5,4}] = Player::light;
+    t.board()[{5,5}] = Player::dark;
+    t.board()[{4,5}] = Player::light;
+    f = t.evaluate_position({6,4});
+    CHECK(f == Position_set{{6,4},{5,4}});
+    f.clear();
+    f = t.evaluate_position({4,6});
+    CHECK(f == Position_set{{4,5}, {4,6}});
+
+    // checking a position with no validity
+    f.clear();
+    f = t.evaluate_position({1,1});
+    CHECK(f.empty());
+}
+
+// tests compute_next_moves_
+TEST_CASE("Possible moves on the board")
+{
+    Model model;
+    Test_access t{model};
+    Position_set f;
+
+    f = t.compute_next();
+    CHECK(f == Position_set{{}});
+    t.board()[{2,2}] = Player::dark;
+    t.board()[{3,2}] = Player::light;
+
+
+    f = t.compute_next();
+    CHECK( f == Position_set{{4,2}});
+}
 //things to test:
 
 //test cases:
