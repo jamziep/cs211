e23932f62c086e63c563b878de56549e2b9f5a0e
seanmorton2023 <63743881+seanmorton2023@users.noreply.github.com>
2021-05-31 21:14:09 -0500

    game over stuff

--- final_project/test/model_test.cxx
+++ final_project/test/model_test.cxx
@@ -12,3 +12,18 @@ TEST_CASE("example test (TODO: replace this)")
 // These tests should demonstrate at least six of the functional
 // requirements.
 //
+
+TEST_CASE("Model::play_move")
+{
+
+    //initialize a model
+    Model m = Model();
+
+    //check the possible moves. current player is white
+    CHECK(m.turn() == Player::white);
+    CHECK(m.winner() == Player::neither);
+
+
+
+}
+
